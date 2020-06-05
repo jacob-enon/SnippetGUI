ccd1685da682a35c3082b5d19dc339c9228a9e67
Jacob Enon <je150221@renishaw.com>
2020-06-05 15:22:03 +0100

    Added Declaration.cs with tests

---
+++ SnippetGUI/Model/Declaration.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SnippetGUI.Model
+{
+    /// <summary>
+    /// A declaration to add to a snippet
+    /// </summary>
+    public class Declaration
+    {
+        /// <summary>
+        /// Unique ID of the declaration
+        /// </summary>
+        public string ID { get; }
+
+        /// <summary>
+        /// Default value to be put in place of the ID
+        /// </summary>
+        public string DefaultValue { get; }
+
+        /// <summary>
+        /// Information on the declaration
+        /// </summary>
+        public string ToolTip { get; }
+
+        /// <summary>
+        /// Construct a declaration
+        /// </summary>
+        /// <param name="id"> Unique ID of the declaration </param>
+        /// <param name="defaultValue"> Default value to be put in place of the ID </param>
+        /// <param name="toolTip"> Information on the declaration </param>
+        public Declaration(string id, string defaultValue, string toolTip = null)
+        {
+            ID = id;
+            DefaultValue = defaultValue;
+            ToolTip = toolTip;
+        }
+
+        /// <summary>
+        /// Determines whether a declaration is valid in the context of the existing declarations
+        /// </summary>
+        /// <param name="existingDeclarations"> Declarations that have already been added </param>
+        /// <param name="ID"> ID of the new declaration </param>
+        /// <param name="defaultValue"> Default value of the new declaration </param>
+        /// <returns> True if the declaration is valid and uniquely identified </returns>
+        public static bool ValidDeclaration(IList<Declaration> existingDeclarations, string ID, string defaultValue)
+            => !(string.IsNullOrEmpty(ID) || string.IsNullOrEmpty(defaultValue))
+                && !existingDeclarations.Where(x => x.ID == ID).Any();
+    }
+}
---
+++ SnippetGUITests/ModelTests/DeclarationTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnippetGUI.Model;
+using System.Collections.Generic;
+
+namespace SnippetGUITests
+{
+    [TestClass]
+    public class DeclarationTests
+    {
+        [TestMethod]
+        public void ValidDeclaration_ReturnsTrue_IfDeclarationIsValidAndNoExisting()
+        {
+            // Arrange
+            var existing = new List<Declaration>();
+
+            // Act
+            var valid = Declaration.ValidDeclaration(existing, "1", "default");
+
+            // Assert
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void ValidDeclaration_ReturnsTrue_IfDeclarationIsValidAndUnique()
+        {
+            // Arrange
+            var existing = new List<Declaration>()
+            {
+                new Declaration("1", "default 1")
+            };
+
+            // Act
+            var valid = Declaration.ValidDeclaration(existing, "2", "default 1");
+
+            // Assert
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void ValidDeclaration_ReturnsFalse_IfIDNotUnique()
+        {
+            // Arrange
+            var ID = "1";
+            var existing = new List<Declaration>()
+            {
+                new Declaration(ID, "default 1")
+            };
+
+            // Act
+            var valid = Declaration.ValidDeclaration(existing, ID, "default 2");
+
+            // Assert
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        [DataRow("", "default")]
+        [DataRow("id", "")]
+        [DataRow("", "")]
+        [DataRow(null, null)]
+        public void ValidDeclaration_ReturnsFalse_IfFieldsNullOrEmpty(string ID, string defaultValue)
+        {
+            // Arrange
+            var existing = new List<Declaration>();
+
+            // Act
+            var valid = Declaration.ValidDeclaration(existing, ID, defaultValue);
+
+            // Assert
+            Assert.IsFalse(valid);
+        }
+    }
+}
